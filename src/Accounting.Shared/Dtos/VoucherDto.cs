8eb3169ac52193d9523b14efe82d9bd50971769e
RezaShafie <r.shafie99@gmail.com>
2026-01-02 10:44:39 +0330

    Added hsared dtos and models

---
+++ src/Accounting.Shared/Dtos/VoucherDto.cs
@@ -0,0 +1,69 @@
+﻿namespace Accounting.Application.Features.Vouchers;
+
+public class VoucherDto
+{
+    public VoucherDto(Guid id,
+        long voucherNumber,
+        DateOnly date,
+        string? description,
+        bool isFinalized,
+        List<VoucherLineDto> lines)
+    {
+        Id = id;
+        VoucherNumber = voucherNumber;
+        Date = date;
+        Description = description;
+        IsFinalized = isFinalized;
+        Lines = lines;
+    }
+
+    public VoucherDto()
+    {
+        Lines = [];
+    }
+    public Guid Id { get; set; }
+    public long VoucherNumber { get; set; }
+    public DateOnly Date { get; set; } 
+    public string? Description { get; set; } 
+    public bool IsFinalized { get; set; }
+    public List<VoucherLineDto> Lines { get; set; }
+
+    public void Deconstruct(out Guid Id, out long VoucherNumber, out DateOnly Date, out string? Description, out bool IsPosted, out List<VoucherLineDto> Lines)
+    {
+        Id = this.Id;
+        VoucherNumber = this.VoucherNumber;
+        Date = this.Date;
+        Description = this.Description;
+        IsPosted = this.IsFinalized;
+        Lines = this.Lines;
+    }
+}
+
+public class VoucherLineDto
+{
+    public VoucherLineDto(string accountCode,
+        string description,
+        decimal debit,
+        decimal credit)
+    {
+        AccountCode = accountCode;
+        Description = description;
+        Debit = debit;
+        Credit = credit;
+    }
+    public VoucherLineDto()
+    {
+    }
+    public string AccountCode { get; set; }
+    public string Description { get; set; } 
+    public decimal Debit { get; set; } 
+    public decimal Credit { get; set; }
+
+    public void Deconstruct(out string AccountCode, out string Description, out decimal Debit, out decimal Credit)
+    {
+        AccountCode = this.AccountCode;
+        Description = this.Description;
+        Debit = this.Debit;
+        Credit = this.Credit;
+    }
+}
---
+++ src/Accounting.Shared/Requests/VoucherRequests.cs
@@ -0,0 +1,19 @@
+﻿using Accounting.Application.Features.Vouchers;
+
+namespace Accounting.Shared.Requests;
+
+public class CreateVoucherRequest
+{
+    public long VoucherNumber { get; set; }
+    public DateOnly Date { get; set; }
+    public string? Description { get; set; }
+    public List<VoucherLineDto> Lines { get; set; } = [];
+}
+
+public class UpdateVoucherRequest
+{
+    public Guid Id { get; set; }
+    public DateOnly Date { get; set; }
+    public string? Description { get; set; }
+    public List<VoucherLineDto> Lines { get; set; } = [];
+}
